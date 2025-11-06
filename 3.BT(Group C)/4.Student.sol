f5b998730ecbd1d9787a9554976b74a61513326a
sans-algo <sanskar.pawar2003@gmail.com>
2025-11-06 20:52:59 +0530

    Initial commit

--- 3.BT(Group C)/4.Student.sol
+++ 3.BT(Group C)/4.Student.sol
@@ -3,36 +3,28 @@ pragma solidity ^0.8.0;
 
 contract StudentData {
     struct Student {
-        uint rollNo;
+        uint256 id;
         string name;
-        uint age;
+        uint256 age;
         string course;
     }
 
     Student[] public students;
 
-    function addStudent(uint _rollNo, string memory _name, uint _age, string memory _course) public {
-        Student memory newStudent = Student(_rollNo, _name, _age, _course);
-        students.push(newStudent);
+    function addStudent(uint256 _id, string calldata _name, uint256 _age, string calldata _course) external {
+        students.push(Student(_id, _name, _age, _course));
     }
 
-    function getStudent(uint index) public view returns (uint, string memory, uint, string memory) {
-        require(index < students.length, "Invalid index");
+    function getStudent(uint256 index) external view returns (uint256, string memory, uint256, string memory) {
         Student memory s = students[index];
-        return (s.rollNo, s.name, s.age, s.course);
+        return (s.id, s.name, s.age, s.course);
     }
 
-    function getStudentCount() public view returns (uint) {
+    function getTotalStudents() external view returns (uint256) {
         return students.length;
     }
 
-    fallback() external payable {
-        emit ReceivedEther(msg.sender, msg.value);
-    }
+    receive() external payable {}
 
-    receive() external payable {
-        emit ReceivedEther(msg.sender, msg.value);
-    }
-
-    event ReceivedEther(address sender, uint amount);
+    fallback() external payable {}
 }
